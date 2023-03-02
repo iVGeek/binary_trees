67a437a8cc2efa776f2d2f71610f4369e87bc8ea
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-03-02 19:51:46 +0300

    Update 134-heap_to_sorted_array.c

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,15 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * heap_to_sorted_array - converts a Binary Max Heap
- * to a sorted array of integers
- * @heap: a pointer to the root node of the heap to convert
- * @size: an address to store the size of the array
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
  *
- * Return: the generated array
- *         NULL on failure
+ * @heap: pointer to the root node of the heap to convert
+ * @size: address to store the size of the array
+ * Return: array sorted
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-
+	return (heap);
 }
