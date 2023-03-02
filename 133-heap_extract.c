a896132faa30c275eea7b6830b53f4b2dff770ac
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-03-03 00:11:20 +0300

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,13 +1,34 @@
 #include "binary_trees.h"
 
-/**
- * heap_extract - extracts the root node of a Max Binary Heap
- * @root: a double pointer to the root node of heap
- *
- * Return: the value stored in the root node
- *         0 on failure
- */
-int heap_extract(heap_t **root)
+/* This function builds a Max Binary Heap tree from an array */
+heap_t *array_to_heap(int *array, size_t size)
 {
+    /* Check for null pointer and empty array */
+    if (array == NULL || size == 0)
+        return NULL;
 
+    /* Allocate memory for the new heap */
+    heap_t *heap = malloc(sizeof(heap_t));
+    if (heap == NULL)
+        return NULL;
+
+    /* Initialize the heap */
+    heap->size = size;
+    heap->data = malloc(size * sizeof(int));
+    if (heap->data == NULL)
+    {
+        free(heap);
+        return NULL;
+    }
+
+    /* Copy the array into the heap */
+    for (size_t i = 0; i < size; i++)
+        heap->data[i] = array[i];
+
+    /* Build the heap */
+    for (int i = (heap->size - 2) / 2; i >= 0; i--)
+        heapify(heap, i);
+
+    /* Return a pointer to the root node of the created binary heap */
+    return heap;
 }
